b38d191b39a5eb0b74cd1fb463f93d086a9a49f1
MarcoTulioLana <111143337+MarcoTulioLana@users.noreply.github.com>
2025-09-08 10:10:03 -0300

    Update index.js
    tirei numero da minha casa

--- src/paginas/Contato/index.js
+++ src/paginas/Contato/index.js
@@ -48,7 +48,7 @@ export default function Contato() {
             título="Dados Pessoais"
         >
             <p className="paragrafo"><span className="negrito">Nome Completo:</span> Marco Tulio Florentino Lana.</p>
-            <p className="paragrafo"><span className="negrito">Endereço:</span> Rua Coronel Emilio Martins, nº 26 Ponte Nova – Minas Gerais – CEP: 35430-249.</p>
+            <p className="paragrafo"><span className="negrito">Endereço:</span> Rua Coronel Emilio Martins Ponte Nova – Minas Gerais – CEP: 35430-249.</p>
             <p className="paragrafo"><span className="negrito">Telefones:</span> (31) 984282129 (vivo) | (31) 38175261 (Fixo) – Recado.</p>
             <p className="paragrafo"><span className="negrito">Email:</span> marcotulioflana@hotmail.com</p>
             <p className="paragrafo"><span className="negrito">Whatsapp:</span><a href="https://wa.me/qr/GZ535TOJA32IL1" target="_blank" rel="noopener noreferrer">https://wa.me/qr/GZ535TOJA32IL1</a></p>
